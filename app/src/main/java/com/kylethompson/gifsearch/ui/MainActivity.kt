a65b1b257e53a17ab865e002df3b0fd9051a83ca
Kyle Thompson <KyleThompson002@gmail.com>
2020-12-13 14:57:40 -0600

    Scaffold main activity

---
+++ app/src/main/java/com/kylethompson/gifsearch/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kylethompson.gifsearch.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kylethompson.gifsearch.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
