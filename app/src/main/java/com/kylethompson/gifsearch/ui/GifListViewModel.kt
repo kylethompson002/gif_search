ddce36c93933ab741f47593c3793f9a0129e9b99
Kyle Thompson <KyleThompson002@gmail.com>
2020-12-12 19:17:15 -0600

    Serch in ViewModel

--- app/src/main/java/com/kylethompson/gifsearch/ui/GifListViewModel.kt
+++ app/src/main/java/com/kylethompson/gifsearch/ui/GifListViewModel.kt
@@ -1,8 +1,41 @@
 package com.kylethompson.gifsearch.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.kylethompson.gifsearch.api.TenorApiService
+import com.kylethompson.gifsearch.api.model.Gif
+import com.kylethompson.gifsearch.data.GifPagingSource
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class GifListViewModel(
-    val gifApiService: TenorApiService
-) : ViewModel()
+    private val apiService: TenorApiService
+) : ViewModel() {
+
+    val gifPageLiveData = MutableLiveData<PagingData<Gif>>()
+
+    fun search(query: String) {
+        viewModelScope.launch {
+            getGifPager(query)
+                .flow.cachedIn(viewModelScope)
+                .collect {
+                    gifPageLiveData.value = it
+                }
+        }
+    }
+    
+    private fun getGifPager(query: String): Pager<String, Gif> {
+        return Pager(
+            // Configure how data is loaded by passing additional properties to
+            // PagingConfig, such as prefetchDistance.
+            PagingConfig(pageSize = 10)
+        ) {
+            GifPagingSource(apiService = apiService, query = query)
+        }
+    }
+}
