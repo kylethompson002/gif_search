061a15ec26d7cd4e425ae70409113c4751bee26a
Kyle Thompson <KyleThompson002@gmail.com>
2020-12-16 14:44:24 -0600

    Refactor view model

--- app/src/main/java/com/kylethompson/gifsearch/ui/search/SearchFragment.kt
+++ app/src/main/java/com/kylethompson/gifsearch/ui/search/SearchFragment.kt
@@ -6,14 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kylethompson.gifsearch.databinding.FragmentSearchBinding
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment() {
@@ -38,27 +34,16 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.searchEditText.doAfterTextChanged { text ->
-            search(text?.toString() ?: "")
+            viewModel.search(text?.toString() ?: "")
         }
 
         binding.gifRecycler.apply {
             layoutManager = LinearLayoutManager(view.context)
             adapter = gifAdapter
         }
-    }
 
-    private var searchJob: Job? = null
-    private fun search(query: String) {
-        if (query.isNotEmpty()) {
-            binding.gifRecycler.scrollToPosition(0)
-
-            // Make sure we cancel the previous job before creating a new one
-            searchJob?.cancel()
-            searchJob = lifecycleScope.launch {
-                viewModel.searchGifs(query).collectLatest {
-                    gifAdapter.submitData(it)
-                }
-            }
+        viewModel.searchResults.observe(viewLifecycleOwner) {
+            gifAdapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
     }
 
--- app/src/main/java/com/kylethompson/gifsearch/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/kylethompson/gifsearch/ui/search/SearchViewModel.kt
@@ -1,5 +1,7 @@
 package com.kylethompson.gifsearch.ui.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -9,25 +11,35 @@ import androidx.paging.cachedIn
 import com.kylethompson.gifsearch.api.TenorApiService
 import com.kylethompson.gifsearch.api.model.Gif
 import com.kylethompson.gifsearch.data.GifPagingSource
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
+@FlowPreview
+@ExperimentalCoroutinesApi
 class SearchViewModel(
     private val apiService: TenorApiService
 ) : ViewModel() {
 
-    private var currentQueryValue: String? = null
-    private var currentSearchResult: Flow<PagingData<Gif>>? = null
+    private val _searchInput = MutableStateFlow("")
+    private val _searchResults = MutableLiveData<PagingData<Gif>>()
+    val searchResults: LiveData<PagingData<Gif>> get() = _searchResults
 
-    fun searchGifs(queryString: String): Flow<PagingData<Gif>> {
-        val lastResult = currentSearchResult
-        if (queryString == currentQueryValue && lastResult != null) {
-            return lastResult
+    init {
+        viewModelScope.launch {
+            _searchInput
+                .debounce(500)
+                .flatMapLatest { getSearchResultStream(it) }
+                .cachedIn(viewModelScope)
+                .collectLatest {
+                    _searchResults.postValue(it)
+                }
         }
-        currentQueryValue = queryString
-        val newResult: Flow<PagingData<Gif>> = getSearchResultStream(queryString)
-            .cachedIn(viewModelScope)
-        currentSearchResult = newResult
-        return newResult
+    }
+
+    fun search(query: String) {
+        _searchInput.value = query
     }
 
     private fun getSearchResultStream(query: String): Flow<PagingData<Gif>> {
