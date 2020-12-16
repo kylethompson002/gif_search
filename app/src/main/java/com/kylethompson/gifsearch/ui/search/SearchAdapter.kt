613a18bb56fe36c4d0eadec8ada2a691dafe33dd
Kyle Thompson <KyleThompson002@gmail.com>
2020-12-16 13:37:10 -0600

    Use full gif size, remove kotlin synthetics

--- app/src/main/java/com/kylethompson/gifsearch/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/kylethompson/gifsearch/ui/search/SearchAdapter.kt
@@ -2,11 +2,15 @@ package com.kylethompson.gifsearch.ui.search
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.kylethompson.gifsearch.R
 import com.kylethompson.gifsearch.api.model.Gif
 import com.kylethompson.gifsearch.api.model.GifDiffCallback
+import com.kylethompson.gifsearch.api.model.MediaItem
 import com.kylethompson.gifsearch.databinding.ItemGifBinding
 
 class SearchAdapter : PagingDataAdapter<Gif, GifViewHolder>(GifDiffCallback()) {
@@ -22,11 +26,22 @@ class SearchAdapter : PagingDataAdapter<Gif, GifViewHolder>(GifDiffCallback()) {
 }
 
 class GifViewHolder(private val binding: ItemGifBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(gif: Gif) {
-        Glide.with(itemView.context)
-            .load(gif.media.first().tinyGif.previewUrl)
-            .into(binding.image)
 
+    fun bind(gif: Gif) {
+        bindMediaItem(gif.media.first().gif)
         binding.title.text = gif.title
+    }
+
+    private fun bindMediaItem(item: MediaItem) {
+        with(binding.image) {
+            updateLayoutParams<ConstraintLayout.LayoutParams> {
+                width = item.dimensions[0]
+                height = item.dimensions[1]
+            }
+            Glide.with(this.context)
+                .load(item.url)
+                .placeholder(R.drawable.ic_image)
+                .into(binding.image)
+        }
     }
 }
